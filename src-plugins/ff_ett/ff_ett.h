075ad24a6b8d207e32347db5c775226a6e3a714d
Henning Rogge <henning.rogge@fkie.fraunhofer.de>
2013-06-04 09:31:45 +0200

    Remove ETX metric from code and make ETT default

--- src-plugins/ff_ett/ff_ett.h
+++ src-plugins/ff_ett/ff_ett.h
@@ -45,7 +45,7 @@
 #include "common/common_types.h"
 #include "core/oonf_subsystem.h"
 
-#define LOG_FF_ETT olsrv2_ffetx_subsystem.logging
-EXPORT extern struct oonf_subsystem olsrv2_ffetx_subsystem;
+#define LOG_FF_ETT olsrv2_ffett_subsystem.logging
+EXPORT extern struct oonf_subsystem olsrv2_ffett_subsystem;
 
 #endif /* FF_ETX_H_ */
