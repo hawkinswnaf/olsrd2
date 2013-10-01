45faa50c5572db61c95241c6fd62c4fc8d485f6f
Will Hawkins <hawkinsw@opentechinstitute.org>
2013-10-01 15:33:46 -0400

    Fix generate_builddata.cmake for version check.

--- cmake/generate_builddata.cmake
+++ cmake/generate_builddata.cmake
@@ -9,11 +9,12 @@ SET(OONF_APP_GIT "cannot read git repository")
 IF(NOT ${found_git} STREQUAL "found_git-NOTFOUND")
 	# get git description WITH dirty flag
 	execute_process(COMMAND git describe --always --long --tags --dirty --match "v[0-9]*"
-		OUTPUT_VARIABLE OONF_APP_GIT OUTPUT_STRIP_TRAILING_WHITESPACE)
+		OUTPUT_VARIABLE OONF_APP_GIT_TAG OUTPUT_STRIP_TRAILING_WHITESPACE)
+	STRING(REGEX REPLACE "^v(.*)-[^-]*-[^-]*$" "\\1" OONF_APP_GIT ${OONF_APP_GIT_TAG})
 ENDIF()
 
 # compare with version number
-STRING(REGEX MATCH "v${OONF_APP_VERSION}-" FOUND_VERSION ${OONF_APP_GIT})
+STRING(REGEX MATCH "${OONF_APP_VERSION}" FOUND_VERSION ${OONF_APP_GIT})
 IF (NOT FOUND_VERSION)
 	message (FATAL_ERROR "App version '${OONF_APP_VERSION}'"
 		" is not present in git description '${OONF_APP_GIT}'."
